59de26c4e670b8cba14091f37b19c369b3d031af
dmitrii92 <dmitrii92>
2020-01-21 17:15:34 +0300

    REST API test stubs added.

---
+++ module/JepRiaShowcase/App/service-rest/src/test/java/com/technology/jep/jepriashowcase/feature/rest/FeatureJaxrsAdapterSimpleTest.java
@@ -0,0 +1,41 @@
+package com.technology.jep.jepriashowcase.feature.rest;
+
+import io.restassured.RestAssured;
+import org.junit.Test;
+
+public class FeatureJaxrsAdapterSimpleTest {
+
+  private static final String baseUrl          = "http://localhost:8080/jepriashowcase-service-rest/api";
+  private static final String operatorName     = "nagornyys";
+  private static final String operatorPassword = "123";
+
+  private void testValidateGetResponse(String testUrl) {
+    RestAssured.given()
+        .auth()
+        .basic(operatorName, operatorPassword)
+        .when()
+        .get(testUrl)
+        .then()
+        .assertThat()
+        .statusCode(200);
+  }
+
+  @Test
+  public void getFeatureOperator() {
+    String testUrl = baseUrl + "/feature/option/feature-operator";
+    testValidateGetResponse(testUrl);
+  }
+
+  @Test
+  public void getFeatureStatus() {
+    String testUrl = baseUrl + "/feature/option/feature-status";
+    testValidateGetResponse(testUrl);
+  }
+
+  @Test
+  public void getRecordByIdSimpleTest() {
+    String testUrl = baseUrl + "/feature/1";
+    testValidateGetResponse(testUrl);
+  }
+
+}
