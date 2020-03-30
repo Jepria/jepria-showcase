c1c169f637b7541e6484d20f027f9bb55805153a
Dobrynin91 <dobrynin.ph@yandex.ru>
2020-03-30 11:03:34 +0300

    [*] ST-71 Разработка системы интеграционного тестирования JepriaShowcase/Feature UPD 2.0 Added TestConstant.java

---
+++ module/JepRiaShowcase/App/service-rest/src/test/java/com/technology/jep/jepriashowcase/feature/rest/TestConstant.java
@@ -0,0 +1,8 @@
+package com.technology.jep.jepriashowcase.feature.rest;
+
+public class TestConstant {
+
+  public static final String FEATURE_URL = "http://localhost:8080/jepriashowcase-service-rest/api/feature";
+  public static final String OPERATOR_NAME = "name";
+  public static final String OPERATOR_PASSWORD = "password";
+}
