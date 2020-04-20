166c52cb2adcc2b8ae4e9bcde78713f027b50744
Dobrynin91 <dobrynin.ph@yandex.ru>
2020-04-20 15:23:12 +0300

    [*] ST-71 Разработка системы интеграционного тестирования JepriaShowcase/Feature UPD 2.0 Внесение изменений

--- module/JepRiaShowcase/App/service-rest/src/test/java/com/technology/jep/jepriashowcase/feature/rest/FeatureJaxrsAdapterTestIT.java
+++ module/JepRiaShowcase/App/service-rest/src/test/java/com/technology/jep/jepriashowcase/feature/rest/FeatureJaxrsAdapterTestIT.java
@@ -12,32 +12,37 @@ import io.restassured.specification.RequestSpecification;
 import org.jepria.server.data.OptionDto;
 import org.jepria.server.data.SearchRequestDto;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import javax.ws.rs.core.MediaType;
 import java.util.*;
 import java.util.stream.IntStream;
 
-import static com.technology.jep.jepriashowcase.feature.rest.TestConstant.*;
-
 public class FeatureJaxrsAdapterTestIT {
 
+  private static final String ENTITY_URL = "http://localhost:8080/jepriashowcase-service-rest/api/feature";
+  private static final String OPERATOR_NAME = "name";
+  private static final String OPERATOR_PASSWORD = "password";
+
+  @Disabled
   @Test
   public void getRecordByIdTest() {
     Integer recordId = 1; //TODO: set record ID for test
 
-    String testUrl = FEATURE_URL + "/feature/" + recordId;
+    String testUrl = ENTITY_URL + "/feature/" + recordId;
     Response response = RestAssured.given().auth().basic(OPERATOR_NAME, OPERATOR_PASSWORD).when().get(testUrl);
     Assertions.assertEquals(200, response.statusCode());
 
     //TODO: check response DTO
   }
 
+  @Disabled
   @Test
   public void updateRecordTest() {
     Integer recordId = 0; //TODO: set record ID for test
 
-    String testUrl = FEATURE_URL + "/" + recordId;
+    String testUrl = ENTITY_URL + "/" + recordId;
 
     FeatureDto testDto = new FeatureDto();
     //TODO: set DTO fields
@@ -55,11 +60,12 @@ public class FeatureJaxrsAdapterTestIT {
     //TODO: check response DTO
   }
 
+  @Disabled
   @Test
   public void deleteRecordTest() {
     Integer recordId = 0; //TODO: set record ID for test
 
-    String testUrl = FEATURE_URL + "/" + recordId;
+    String testUrl = ENTITY_URL + "/" + recordId;
 
     RequestSpecification requestDelete =
             RestAssured.given().auth().basic(OPERATOR_NAME, OPERATOR_PASSWORD);
@@ -70,9 +76,10 @@ public class FeatureJaxrsAdapterTestIT {
     Assertions.assertEquals(200, statusCode); //TODO: maybe should be 204
   }
 
+  @Disabled
   @Test
   public void createRecordTest() {
-    String testUrl = FEATURE_URL;
+    String testUrl = ENTITY_URL;
 
     FeatureDto testDto = new FeatureDto();
     //TODO: set DTO fields
@@ -88,9 +95,10 @@ public class FeatureJaxrsAdapterTestIT {
     Assertions.assertEquals(201, statusCode);
   }
 
+  @Disabled
   @Test
   public void getFeatureOperatorTest() {
-    String testUrl = FEATURE_URL + "/option/feature-operator";
+    String testUrl = ENTITY_URL + "/option/feature-operator";
 
     RestAssured.given()
             .auth()
@@ -102,9 +110,10 @@ public class FeatureJaxrsAdapterTestIT {
             .statusCode(200);
   }
 
+  @Disabled
   @Test
   public void getFeatureStatusTest() {
-    String testUrl = FEATURE_URL + "/option/feature-status";
+    String testUrl = ENTITY_URL + "/option/feature-status";
 
     RestAssured.given()
             .auth()
@@ -116,12 +125,13 @@ public class FeatureJaxrsAdapterTestIT {
             .statusCode(200);
   }
 
+  @Disabled
   @Test
   public void setFeatureResponsibleTest() {
     Integer recordId = 1; //TODO: set record ID for test
     Integer responsibleId = 1;
 
-    String testUrl = FEATURE_URL + "/" + recordId + "/set-feature-responsible?responsibleId=" + responsibleId;
+    String testUrl = ENTITY_URL + "/" + recordId + "/set-feature-responsible?responsibleId=" + responsibleId;
 
     RequestSpecification requestPost =
             RestAssured.given().auth().basic(OPERATOR_NAME, OPERATOR_PASSWORD);
@@ -139,14 +149,14 @@ public class FeatureJaxrsAdapterTestIT {
                     .header("Content-Type", "application/json")
                     .body(testDto);
 
-    Response postResponse = requestPost.post(FEATURE_URL);
+    Response postResponse = requestPost.post(ENTITY_URL);
 
     Assertions.assertEquals(201, postResponse.statusCode());
     return postResponse.getHeader("Location");
   }
 
   private void updateRecord(FeatureDto testDto, Integer recordId) {
-    updateRecord(testDto, FEATURE_URL + "/" + recordId);
+    updateRecord(testDto, ENTITY_URL + "/" + recordId);
   }
 
   private void updateRecord(FeatureDto testDto, String location) {
@@ -168,12 +178,103 @@ public class FeatureJaxrsAdapterTestIT {
   }
 
   private FeatureDto getRecord(Integer featureId) {
-    String testUrl = FEATURE_URL + "/feature/" + featureId;
+    String testUrl = ENTITY_URL + "/feature/" + featureId;
     return getRecord(testUrl);
   }
 
   private void deleteRecord(int featureId) {
-    deleteRecord(FEATURE_URL + "/" + featureId);
+    deleteRecord(ENTITY_URL + "/" + featureId);
+  }
+
+
+  @Test
+  public void featureCrudTest() {
+    FeatureDto dto = createRandomMinimalFeatureDto();
+    String location = createRecord(dto);
+    FeatureDto createdDto = setMinimalFeatureDto(getRecord(location));
+    Assertions.assertTrue(equalsEntities(dto, createdDto), "New dto and created dto are different");
+
+    FeatureDto newDto = createRandomMinimalFeatureDto();
+    updateRecord(newDto, location);
+    FeatureDto updatedDto = setMinimalFeatureDto(getRecord(location));
+    Assertions.assertTrue(equalsEntities(newDto, updatedDto), "New dto for updating and updated dto are different");
+
+    deleteRecord(location);
+  }
+
+  private boolean equalsEntities(Object obj1, Object obj2) {
+    if (obj1 == obj2) {
+      return true;
+    }
+
+    FeatureDto entity1 = (FeatureDto) obj1;
+    FeatureDto entity2 = (FeatureDto) obj2;
+
+    if (!Objects.equals(entity1.getFeatureId(), entity2.getFeatureId())) {
+      return false;
+    }
+    if (!Objects.equals(entity1.getFeatureName(), entity2.getFeatureName())) {
+      return false;
+    }
+    if (!Objects.equals(entity1.getFeatureNameEn(), entity2.getFeatureNameEn())) {
+      return false;
+    }
+    if (!Objects.equals(entity1.getDescription(), entity2.getDescription())) {
+      return false;
+    }
+    if (!Objects.equals(entity1.getFeatureStatus(), entity2.getFeatureStatus())) {
+      return false;
+    }
+    if (!Objects.equals(entity1.getDateIns(), entity2.getDateIns())) {
+      return false;
+    }
+    if (!Objects.equals(entity1.getAuthor(), entity2.getAuthor())) {
+      return false;
+    }
+    return Objects.equals(entity1.getResponsible(), entity2.getResponsible());
+  }
+
+  private FeatureDto createRandomFullFeatureDto() {
+    FeatureDto featureDto = new FeatureDto();
+    featureDto.setFeatureName(getGenerateFeatureString(10, false));
+    featureDto.setFeatureNameEn(getGenerateFeatureString(10, false));
+    featureDto.setDescription(getGenerateFeatureString(10, false));
+    featureDto.setFeatureStatus(getGenerateOptionDtoString(getGenerateFeatureString(10, false)));
+    featureDto.setDateIns(new Date(new Random().nextLong()));
+    featureDto.setAuthor(getGenerateOptionDtoString(new Random().nextInt(1000)));
+    featureDto.setResponsible(getGenerateOptionDtoString(new Random().nextInt(1000)));
+    return featureDto;
+  }
+
+  private FeatureDto createRandomMinimalFeatureDto() {
+    FeatureDto featureDto = new FeatureDto();
+    featureDto.setFeatureName(getGenerateFeatureString(10, false));
+    featureDto.setFeatureNameEn(getGenerateFeatureString(10, false));
+    featureDto.setDescription(getGenerateFeatureString(10, false));
+    return featureDto;
+  }
+
+  private FeatureDto setMinimalFeatureDto(FeatureDto dto) {
+    dto.setFeatureId(null);
+    dto.setFeatureStatus(null);
+    dto.setDateIns(null);
+    dto.setAuthor(null);
+    dto.setResponsible(null);
+    return dto;
+  }
+
+  private String getGenerateFeatureString(int length, boolean russian) {
+    String alphabet = russian ? "АБВГДЕЁЖЭИЙКЛМНОПРСТУФХЦЧШЭЮЯ" : "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    StringBuilder sb = new StringBuilder();
+    IntStream.range(0, length).forEach(i -> sb.append(alphabet.charAt((int) (Math.random() * alphabet.length()))));
+    return sb.toString();
+  }
+
+  private <T> OptionDto<T> getGenerateOptionDtoString(T value) {
+    OptionDto<T> optionDto = new OptionDto<>();
+    optionDto.setName(getGenerateFeatureString(5, true));
+    optionDto.setValue(value);
+    return optionDto;
   }
 
   private void deleteRecord(String location) {
@@ -210,64 +311,7 @@ public class FeatureJaxrsAdapterTestIT {
     return list;
   }
 
-  @Test
-  public void featureCrudTest() {
-    FeatureDto dto = createRandomMinimalFeatureDto();
-    String location = createRecord(dto);
-    FeatureDto createdDto = setMinimalFeatureDto(getRecord(location));
-    Assertions.assertEquals(dto, createdDto, "New dto and created dto are different");
-
-    FeatureDto newDto = createRandomMinimalFeatureDto();
-    updateRecord(newDto, location);
-    FeatureDto updatedDto = setMinimalFeatureDto(getRecord(location));
-    Assertions.assertEquals(newDto, updatedDto, "New dto for updating and updated dto are different");
-
-    deleteRecord(location);
-  }
-
-  private FeatureDto createRandomFullFeatureDto() {
-    FeatureDto featureDto = new FeatureDto();
-    featureDto.setFeatureName(getGenerateFeatureString(10, false));
-    featureDto.setFeatureNameEn(getGenerateFeatureString(10, false));
-    featureDto.setDescription(getGenerateFeatureString(10, false));
-    featureDto.setFeatureStatus(getGenerateOptionDtoString(getGenerateFeatureString(10, false)));
-    featureDto.setDateIns(new Date(new Random().nextLong()));
-    featureDto.setAuthor(getGenerateOptionDtoString(new Random().nextInt(1000)));
-    featureDto.setResponsible(getGenerateOptionDtoString(new Random().nextInt(1000)));
-    return featureDto;
-  }
-
-  private FeatureDto createRandomMinimalFeatureDto() {
-    FeatureDto featureDto = new FeatureDto();
-    featureDto.setFeatureName(getGenerateFeatureString(10, false));
-    featureDto.setFeatureNameEn(getGenerateFeatureString(10, false));
-    featureDto.setDescription(getGenerateFeatureString(10, false));
-    return featureDto;
-  }
-
-  private FeatureDto setMinimalFeatureDto(FeatureDto dto) {
-    dto.setFeatureId(null);
-    dto.setFeatureStatus(null);
-    dto.setDateIns(null);
-    dto.setAuthor(null);
-    dto.setResponsible(null);
-    return dto;
-  }
-
-  private String getGenerateFeatureString(int length, boolean russian) {
-    String alphabet = russian ? "АБВГДЕЁЖЭИЙКЛМНОПРСТУФХЦЧШЭЮЯ" : "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    StringBuilder sb = new StringBuilder();
-    IntStream.range(0, length).forEach(i -> sb.append(alphabet.charAt(new Random().nextInt(alphabet.length()))));
-    return sb.toString();
-  }
-
-  private <T> OptionDto<T> getGenerateOptionDtoString(T value) {
-    OptionDto<T> optionDto = new OptionDto<>();
-    optionDto.setName(getGenerateFeatureString(5, true));
-    optionDto.setValue(value);
-    return optionDto;
-  }
-
+  @Disabled
   @Test
   public void featureGetTest() {
     FeatureDto createdDto = getRecord("sasfaf");
@@ -321,9 +365,10 @@ public class FeatureJaxrsAdapterTestIT {
     return gson.fromJson(response.asString(), FeatureDto.class);
   }
 
+  @Disabled
   @Test
   public void featureSearchTest() {
-    String testUrl = FEATURE_URL + "/search";
+    String testUrl = ENTITY_URL + "/search";
 
     SearchRequestDto<FeatureSearchDto> searchRequestDto = new SearchRequestDto<>();
 
