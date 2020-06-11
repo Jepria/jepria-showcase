9cc3e2fec428bf8ca62122f071bb3922b9c40dea
Dobrynin91 <dobrynin.ph@yandex.ru>
2020-06-11 13:41:07 +0300

    created class for the CRUD tests

---
+++ module/JepRiaShowcase/App/service-rest/src/test/java/com/technology/jep/jepriashowcase/feature/rest/FeatureJaxrsAdapterCrudTestIT.java
@@ -0,0 +1,163 @@
+package com.technology.jep.jepriashowcase.feature.rest;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.technology.jep.jepriashowcase.feature.dto.FeatureDto;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.IntStream;
+
+public class FeatureJaxrsAdapterCrudTestIT {
+
+  private static final String ENTITY_URL = "http://localhost:8080/jepriashowcase-service-rest/api/feature";
+  private static final String OPERATOR_NAME = "name";
+  private static final String OPERATOR_PASSWORD = "password";
+
+  @Test
+  public void featureCrudTest() {
+    FeatureDto dto = createRandomMinimalFeatureDto();
+    String location = createFeature(dto);
+    FeatureDto createdDto = setMinimalFeatureDto(getFeature(location));
+    Assertions.assertTrue(equalsEntities(dto, createdDto), "New dto and created dto are different");
+
+    FeatureDto newDto = createRandomMinimalFeatureDto();
+    updateFeature(newDto, location);
+    FeatureDto updatedDto = setMinimalFeatureDto(getFeature(location));
+    Assertions.assertTrue(equalsEntities(newDto, updatedDto), "New dto for updating and updated dto are different");
+
+    deleteFeature(location);
+  }
+
+  private String createFeature(FeatureDto testDto) {
+    Response postResponse = RestAssured.given()
+            .auth()
+            .basic(OPERATOR_NAME, OPERATOR_PASSWORD)
+            .header("Content-Type", "application/json")
+            .body(testDto)
+            .post(ENTITY_URL);
+    Assertions.assertEquals(201, postResponse.statusCode());
+    return postResponse.getHeader("Location");
+  }
+
+  private FeatureDto getFeature(String location) {
+    Response response = RestAssured.given()
+            .auth()
+            .basic(OPERATOR_NAME, OPERATOR_PASSWORD)
+            .when()
+            .get(location);
+    Assertions.assertEquals(200, response.statusCode());
+    Gson gson = new GsonBuilder().setDateFormat("dd.MM.yyyy").create();
+    return gson.fromJson(response.asString(), FeatureDto.class);
+  }
+
+  private void updateFeature(FeatureDto testDto, String location) {
+    Response putResponse = RestAssured.given()
+            .auth()
+            .basic(OPERATOR_NAME, OPERATOR_PASSWORD)
+            .header("Content-Type", "application/json")
+            .body(testDto)
+            .put(location);
+    int statusCode = putResponse.statusCode();
+    Assertions.assertEquals(200, statusCode);
+  }
+
+  private void deleteFeature(String location) {
+    RequestSpecification requestDelete = RestAssured.given()
+            .auth()
+            .basic(OPERATOR_NAME, OPERATOR_PASSWORD);
+    List<Integer> featureProcessIdList = getFeatureProcessIdList(location);
+    featureProcessIdList.sort(Comparator.reverseOrder());
+    featureProcessIdList.forEach(featureProcessId -> {
+      requestDelete.delete(location + "/feature-process/" + featureProcessId);
+    });
+    Response deleteResponse = requestDelete.delete(location);
+    Assertions.assertEquals(200, deleteResponse.statusCode()); //TODO: maybe should be 204
+  }
+
+  private List<Integer> getFeatureProcessIdList(String location) {
+    Response response = RestAssured.given()
+            .auth()
+            .basic(OPERATOR_NAME, OPERATOR_PASSWORD)
+            .when()
+            .get(location + "/feature-process");
+    JsonElement responseAsJsonElement = new Gson().fromJson(response.asString(), JsonElement.class);
+    List<Integer> list = new ArrayList<>();
+    if (!responseAsJsonElement.isJsonNull() && responseAsJsonElement.isJsonArray()) {
+      responseAsJsonElement.getAsJsonArray().forEach(json -> {
+        if (json.isJsonObject()) {
+          JsonObject jsonOb = json.getAsJsonObject();
+          int featureProcessId = jsonOb.get("featureProcessId").getAsInt();
+          list.add(featureProcessId);
+        }
+      });
+      return list;
+    }
+    return list;
+  }
+
+  private FeatureDto createRandomMinimalFeatureDto() {
+    FeatureDto featureDto = new FeatureDto();
+    featureDto.setFeatureName(getGenerateFeatureString(10, true));
+    featureDto.setFeatureNameEn(getGenerateFeatureString(10, true));
+    featureDto.setDescription(getGenerateFeatureString(10, true));
+    return featureDto;
+  }
+
+  private FeatureDto setMinimalFeatureDto(FeatureDto dto) {
+    dto.setFeatureId(null);
+    dto.setFeatureStatus(null);
+    dto.setDateIns(null);
+    dto.setAuthor(null);
+    dto.setResponsible(null);
+    return dto;
+  }
+
+  private String getGenerateFeatureString(int length, boolean russian) {
+    String alphabet = russian ? "АБВГДЕЁЖЭИЙКЛМНОПРСТУФХЦЧШЭЮЯ" : "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    StringBuilder sb = new StringBuilder();
+    IntStream.range(0, length).forEach(i -> sb.append(alphabet.charAt((int) (Math.random() * alphabet.length()))));
+    return sb.toString();
+  }
+
+  private boolean equalsEntities(Object obj1, Object obj2) {
+    if (obj1 == obj2) {
+      return true;
+    }
+
+    FeatureDto entity1 = (FeatureDto) obj1;
+    FeatureDto entity2 = (FeatureDto) obj2;
+
+    if (!Objects.equals(entity1.getFeatureId(), entity2.getFeatureId())) {
+      return false;
+    }
+    if (!Objects.equals(entity1.getFeatureName(), entity2.getFeatureName())) {
+      return false;
+    }
+    if (!Objects.equals(entity1.getFeatureNameEn(), entity2.getFeatureNameEn())) {
+      return false;
+    }
+    if (!Objects.equals(entity1.getDescription(), entity2.getDescription())) {
+      return false;
+    }
+    if (!Objects.equals(entity1.getFeatureStatus(), entity2.getFeatureStatus())) {
+      return false;
+    }
+    if (!Objects.equals(entity1.getDateIns(), entity2.getDateIns())) {
+      return false;
+    }
+    if (!Objects.equals(entity1.getAuthor(), entity2.getAuthor())) {
+      return false;
+    }
+    return Objects.equals(entity1.getResponsible(), entity2.getResponsible());
+  }
+}
--- module/JepRiaShowcase/App/service-rest/src/test/java/com/technology/jep/jepriashowcase/feature/rest/FeatureJaxrsAdapterTestIT.java
+++ module/JepRiaShowcase/App/service-rest/src/test/java/com/technology/jep/jepriashowcase/feature/rest/FeatureJaxrsAdapterTestIT.java
@@ -2,22 +2,17 @@ package com.technology.jep.jepriashowcase.feature.rest;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 import com.technology.jep.jepriashowcase.feature.dto.FeatureDto;
 import com.technology.jep.jepriashowcase.feature.dto.FeatureSearchDto;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
-import org.jepria.server.data.OptionDto;
 import org.jepria.server.data.SearchRequestDto;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import javax.ws.rs.core.MediaType;
-import java.util.*;
-import java.util.stream.IntStream;
 
 public class FeatureJaxrsAdapterTestIT {
 
@@ -142,182 +137,6 @@ public class FeatureJaxrsAdapterTestIT {
     Assertions.assertEquals(200, statusCode);
   }
 
-  private String createRecord(FeatureDto testDto) {
-    RequestSpecification requestPost =
-            RestAssured.given().auth()
-                    .basic(OPERATOR_NAME, OPERATOR_PASSWORD)
-                    .header("Content-Type", "application/json")
-                    .body(testDto);
-
-    Response postResponse = requestPost.post(ENTITY_URL);
-
-    Assertions.assertEquals(201, postResponse.statusCode());
-    return postResponse.getHeader("Location");
-  }
-
-  private void updateRecord(FeatureDto testDto, Integer recordId) {
-    updateRecord(testDto, ENTITY_URL + "/" + recordId);
-  }
-
-  private void updateRecord(FeatureDto testDto, String location) {
-    RequestSpecification requestPost =
-            RestAssured.given().auth().basic(OPERATOR_NAME, OPERATOR_PASSWORD)
-                    .header("Content-Type", "application/json")
-                    .body(testDto);
-    Response postResponse = requestPost.put(location);
-
-    int statusCode = postResponse.statusCode();
-    Assertions.assertEquals(200, statusCode);
-  }
-
-  private FeatureDto getRecord(String location) {
-    Response response = RestAssured.given().auth().basic(OPERATOR_NAME, OPERATOR_PASSWORD).when().get(location);
-    Assertions.assertEquals(200, response.statusCode());
-    Gson gson = new GsonBuilder().setDateFormat("dd.MM.yyyy").create();
-    return gson.fromJson(response.asString(), FeatureDto.class);
-  }
-
-  private FeatureDto getRecord(Integer featureId) {
-    String testUrl = ENTITY_URL + "/feature/" + featureId;
-    return getRecord(testUrl);
-  }
-
-  private void deleteRecord(int featureId) {
-    deleteRecord(ENTITY_URL + "/" + featureId);
-  }
-
-
-  @Test
-  public void featureCrudTest() {
-    FeatureDto dto = createRandomMinimalFeatureDto();
-    String location = createRecord(dto);
-    FeatureDto createdDto = setMinimalFeatureDto(getRecord(location));
-    Assertions.assertTrue(equalsEntities(dto, createdDto), "New dto and created dto are different");
-
-    FeatureDto newDto = createRandomMinimalFeatureDto();
-    updateRecord(newDto, location);
-    FeatureDto updatedDto = setMinimalFeatureDto(getRecord(location));
-    Assertions.assertTrue(equalsEntities(newDto, updatedDto), "New dto for updating and updated dto are different");
-
-    deleteRecord(location);
-  }
-
-  private boolean equalsEntities(Object obj1, Object obj2) {
-    if (obj1 == obj2) {
-      return true;
-    }
-
-    FeatureDto entity1 = (FeatureDto) obj1;
-    FeatureDto entity2 = (FeatureDto) obj2;
-
-    if (!Objects.equals(entity1.getFeatureId(), entity2.getFeatureId())) {
-      return false;
-    }
-    if (!Objects.equals(entity1.getFeatureName(), entity2.getFeatureName())) {
-      return false;
-    }
-    if (!Objects.equals(entity1.getFeatureNameEn(), entity2.getFeatureNameEn())) {
-      return false;
-    }
-    if (!Objects.equals(entity1.getDescription(), entity2.getDescription())) {
-      return false;
-    }
-    if (!Objects.equals(entity1.getFeatureStatus(), entity2.getFeatureStatus())) {
-      return false;
-    }
-    if (!Objects.equals(entity1.getDateIns(), entity2.getDateIns())) {
-      return false;
-    }
-    if (!Objects.equals(entity1.getAuthor(), entity2.getAuthor())) {
-      return false;
-    }
-    return Objects.equals(entity1.getResponsible(), entity2.getResponsible());
-  }
-
-  private FeatureDto createRandomFullFeatureDto() {
-    FeatureDto featureDto = new FeatureDto();
-    featureDto.setFeatureName(getGenerateFeatureString(10, false));
-    featureDto.setFeatureNameEn(getGenerateFeatureString(10, false));
-    featureDto.setDescription(getGenerateFeatureString(10, false));
-    featureDto.setFeatureStatus(getGenerateOptionDtoString(getGenerateFeatureString(10, false)));
-    featureDto.setDateIns(new Date(new Random().nextLong()));
-    featureDto.setAuthor(getGenerateOptionDtoString(new Random().nextInt(1000)));
-    featureDto.setResponsible(getGenerateOptionDtoString(new Random().nextInt(1000)));
-    return featureDto;
-  }
-
-  private FeatureDto createRandomMinimalFeatureDto() {
-    FeatureDto featureDto = new FeatureDto();
-    featureDto.setFeatureName(getGenerateFeatureString(10, false));
-    featureDto.setFeatureNameEn(getGenerateFeatureString(10, false));
-    featureDto.setDescription(getGenerateFeatureString(10, false));
-    return featureDto;
-  }
-
-  private FeatureDto setMinimalFeatureDto(FeatureDto dto) {
-    dto.setFeatureId(null);
-    dto.setFeatureStatus(null);
-    dto.setDateIns(null);
-    dto.setAuthor(null);
-    dto.setResponsible(null);
-    return dto;
-  }
-
-  private String getGenerateFeatureString(int length, boolean russian) {
-    String alphabet = russian ? "АБВГДЕЁЖЭИЙКЛМНОПРСТУФХЦЧШЭЮЯ" : "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    StringBuilder sb = new StringBuilder();
-    IntStream.range(0, length).forEach(i -> sb.append(alphabet.charAt((int) (Math.random() * alphabet.length()))));
-    return sb.toString();
-  }
-
-  private <T> OptionDto<T> getGenerateOptionDtoString(T value) {
-    OptionDto<T> optionDto = new OptionDto<>();
-    optionDto.setName(getGenerateFeatureString(5, true));
-    optionDto.setValue(value);
-    return optionDto;
-  }
-
-  private void deleteRecord(String location) {
-    RequestSpecification requestDelete =
-            RestAssured.given().auth().basic(OPERATOR_NAME, OPERATOR_PASSWORD);
-
-    List<Integer> featureProcessIdList = getFeatureProcessIdList(location);
-    featureProcessIdList.sort(Comparator.reverseOrder());
-    featureProcessIdList.forEach(featureProcessId -> {
-      requestDelete.delete(location + "/feature-process/" + featureProcessId);
-    });
-    Response deleteResponse = requestDelete.delete(location);
-    Assertions.assertEquals(200, deleteResponse.statusCode()); //TODO: maybe should be 204
-  }
-
-  private List<Integer> getFeatureProcessIdList(String location) {
-    Response response = RestAssured.given()
-            .auth()
-            .basic(OPERATOR_NAME, OPERATOR_PASSWORD)
-            .when()
-            .get(location + "/feature-process");
-    JsonElement responseAsJsonElement = new Gson().fromJson(response.asString(), JsonElement.class);
-    List<Integer> list = new ArrayList<>();
-    if (!responseAsJsonElement.isJsonNull() && responseAsJsonElement.isJsonArray()) {
-      responseAsJsonElement.getAsJsonArray().forEach(json -> {
-        if (json.isJsonObject()) {
-          JsonObject jsonOb = json.getAsJsonObject();
-          int featureProcessId = jsonOb.get("featureProcessId").getAsInt();
-          list.add(featureProcessId);
-        }
-      });
-      return list;
-    }
-    return list;
-  }
-
-  @Disabled
-  @Test
-  public void featureGetTest() {
-    FeatureDto createdDto = getRecord("sasfaf");
-
-  }
-
   private String postSearchRequest(SearchRequestDto<FeatureSearchDto> searchRequestDto, String location) {
 
     RequestSpecification request =
@@ -386,5 +205,4 @@ public class FeatureJaxrsAdapterTestIT {
     }
 
   }
-
 }
