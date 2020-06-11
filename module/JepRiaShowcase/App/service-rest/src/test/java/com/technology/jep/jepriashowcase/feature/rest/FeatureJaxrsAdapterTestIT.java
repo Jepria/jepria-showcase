f51c8103d0d85ed759ae0957be23cb52a4e2efc8
remitot <call9096463086@gmail.com>
2020-06-11 13:37:25 +0300

    refactor: simplify tests

--- module/JepRiaShowcase/App/service-rest/src/test/java/com/technology/jep/jepriashowcase/feature/rest/FeatureJaxrsAdapterTestIT.java
+++ module/JepRiaShowcase/App/service-rest/src/test/java/com/technology/jep/jepriashowcase/feature/rest/FeatureJaxrsAdapterTestIT.java
@@ -5,149 +5,32 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.technology.jep.jepriashowcase.feature.dto.FeatureDto;
-import com.technology.jep.jepriashowcase.feature.dto.FeatureSearchDto;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.jepria.server.data.OptionDto;
-import org.jepria.server.data.SearchRequestDto;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import javax.ws.rs.core.MediaType;
 import java.util.*;
 import java.util.stream.IntStream;
 
 public class FeatureJaxrsAdapterTestIT {
 
   private static final String ENTITY_URL = "http://localhost:8080/jepriashowcase-service-rest/api/feature";
-  private static final String OPERATOR_NAME = "name";
+  private static final String OPERATOR_NAME = "username";
   private static final String OPERATOR_PASSWORD = "password";
 
-  @Disabled
-  @Test
-  public void getRecordByIdTest() {
-    Integer recordId = 1; //TODO: set record ID for test
-
-    String testUrl = ENTITY_URL + "/feature/" + recordId;
-    Response response = RestAssured.given().auth().basic(OPERATOR_NAME, OPERATOR_PASSWORD).when().get(testUrl);
-    Assertions.assertEquals(200, response.statusCode());
-
-    //TODO: check response DTO
-  }
-
-  @Disabled
-  @Test
-  public void updateRecordTest() {
-    Integer recordId = 0; //TODO: set record ID for test
-
-    String testUrl = ENTITY_URL + "/" + recordId;
-
-    FeatureDto testDto = new FeatureDto();
-    //TODO: set DTO fields
-
-    RequestSpecification requestPost =
-            RestAssured.given().auth().basic(OPERATOR_NAME, OPERATOR_PASSWORD)
-                    .header("Content-Type", "application/json")
-                    .body(testDto);
-
-    Response postResponse = requestPost.post(testUrl);
-
-    int statusCode = postResponse.statusCode();
-    Assertions.assertEquals(200, statusCode);
-
-    //TODO: check response DTO
-  }
-
-  @Disabled
-  @Test
-  public void deleteRecordTest() {
-    Integer recordId = 0; //TODO: set record ID for test
-
-    String testUrl = ENTITY_URL + "/" + recordId;
-
-    RequestSpecification requestDelete =
-            RestAssured.given().auth().basic(OPERATOR_NAME, OPERATOR_PASSWORD);
-
-    Response deleteResponse = requestDelete.delete(testUrl);
-
-    int statusCode = deleteResponse.statusCode();
-    Assertions.assertEquals(200, statusCode); //TODO: maybe should be 204
-  }
-
-  @Disabled
-  @Test
-  public void createRecordTest() {
-    String testUrl = ENTITY_URL;
-
-    FeatureDto testDto = new FeatureDto();
-    //TODO: set DTO fields
-
-    RequestSpecification requestPost =
-            RestAssured.given().auth().basic(OPERATOR_NAME, OPERATOR_PASSWORD)
-                    .header("Content-Type", "application/json")
-                    .body(testDto);
-
-    Response postResponse = requestPost.post(testUrl);
-
-    int statusCode = postResponse.statusCode();
-    Assertions.assertEquals(201, statusCode);
-  }
-
-  @Disabled
-  @Test
-  public void getFeatureOperatorTest() {
-    String testUrl = ENTITY_URL + "/option/feature-operator";
-
-    RestAssured.given()
-            .auth()
-            .basic(OPERATOR_NAME, OPERATOR_PASSWORD)
-            .when()
-            .get(testUrl)
-            .then()
-            .assertThat()
-            .statusCode(200);
-  }
-
-  @Disabled
-  @Test
-  public void getFeatureStatusTest() {
-    String testUrl = ENTITY_URL + "/option/feature-status";
-
-    RestAssured.given()
-            .auth()
-            .basic(OPERATOR_NAME, OPERATOR_PASSWORD)
-            .when()
-            .get(testUrl)
-            .then()
-            .assertThat()
-            .statusCode(200);
-  }
-
-  @Disabled
-  @Test
-  public void setFeatureResponsibleTest() {
-    Integer recordId = 1; //TODO: set record ID for test
-    Integer responsibleId = 1;
-
-    String testUrl = ENTITY_URL + "/" + recordId + "/set-feature-responsible?responsibleId=" + responsibleId;
-
-    RequestSpecification requestPost =
-            RestAssured.given().auth().basic(OPERATOR_NAME, OPERATOR_PASSWORD);
-
-    Response postResponse = requestPost.post();
-
-    int statusCode = postResponse.statusCode();
-    Assertions.assertEquals(200, statusCode);
-  }
-
-  private String createRecord(FeatureDto testDto) {
+  /**
+   * @param dto
+   * @return {@code Location} response header value
+   */
+  private String createRecord(Object dto) {
     RequestSpecification requestPost =
             RestAssured.given().auth()
                     .basic(OPERATOR_NAME, OPERATOR_PASSWORD)
                     .header("Content-Type", "application/json")
-                    .body(testDto);
+                    .body(dto);
 
     Response postResponse = requestPost.post(ENTITY_URL);
 
@@ -155,60 +38,56 @@ public class FeatureJaxrsAdapterTestIT {
     return postResponse.getHeader("Location");
   }
 
-  private void updateRecord(FeatureDto testDto, Integer recordId) {
-    updateRecord(testDto, ENTITY_URL + "/" + recordId);
-  }
-
-  private void updateRecord(FeatureDto testDto, String location) {
+  private void updateRecord(Object dto, String location) {
     RequestSpecification requestPost =
             RestAssured.given().auth().basic(OPERATOR_NAME, OPERATOR_PASSWORD)
                     .header("Content-Type", "application/json")
-                    .body(testDto);
+                    .body(dto);
     Response postResponse = requestPost.put(location);
 
     int statusCode = postResponse.statusCode();
     Assertions.assertEquals(200, statusCode);
   }
 
-  private FeatureDto getRecord(String location) {
+  private FeatureDto getFeature(String location) {
     Response response = RestAssured.given().auth().basic(OPERATOR_NAME, OPERATOR_PASSWORD).when().get(location);
     Assertions.assertEquals(200, response.statusCode());
-    Gson gson = new GsonBuilder().setDateFormat("dd.MM.yyyy").create();
+    Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd").create();
     return gson.fromJson(response.asString(), FeatureDto.class);
   }
 
-  private FeatureDto getRecord(Integer featureId) {
-    String testUrl = ENTITY_URL + "/feature/" + featureId;
-    return getRecord(testUrl);
-  }
+  private void deleteFeature(String location) {
+    RequestSpecification requestDelete =
+            RestAssured.given().auth().basic(OPERATOR_NAME, OPERATOR_PASSWORD);
 
-  private void deleteRecord(int featureId) {
-    deleteRecord(ENTITY_URL + "/" + featureId);
+    List<Integer> featureProcessIdList = getFeatureProcessIdList(location);
+    featureProcessIdList.sort(Comparator.reverseOrder());
+    featureProcessIdList.forEach(featureProcessId -> {
+      requestDelete.delete(location + "/feature-process/" + featureProcessId);
+    });
+    Response deleteResponse = requestDelete.delete(location);
+    Assertions.assertEquals(200, deleteResponse.statusCode()); //TODO: maybe should be 204
   }
-
 
   @Test
   public void featureCrudTest() {
     FeatureDto dto = createRandomMinimalFeatureDto();
     String location = createRecord(dto);
-    FeatureDto createdDto = setMinimalFeatureDto(getRecord(location));
+    FeatureDto createdDto = setMinimalFeatureDto(getFeature(location));
     Assertions.assertTrue(equalsEntities(dto, createdDto), "New dto and created dto are different");
 
     FeatureDto newDto = createRandomMinimalFeatureDto();
     updateRecord(newDto, location);
-    FeatureDto updatedDto = setMinimalFeatureDto(getRecord(location));
+    FeatureDto updatedDto = setMinimalFeatureDto(getFeature(location));
     Assertions.assertTrue(equalsEntities(newDto, updatedDto), "New dto for updating and updated dto are different");
 
-    deleteRecord(location);
+    deleteFeature(location);
   }
 
-  private boolean equalsEntities(Object obj1, Object obj2) {
-    if (obj1 == obj2) {
+  private boolean equalsEntities(FeatureDto entity1, FeatureDto entity2) {
+    if (entity1 == entity2) {
       return true;
     }
-
-    FeatureDto entity1 = (FeatureDto) obj1;
-    FeatureDto entity2 = (FeatureDto) obj2;
 
     if (!Objects.equals(entity1.getFeatureId(), entity2.getFeatureId())) {
       return false;
@@ -231,7 +110,11 @@ public class FeatureJaxrsAdapterTestIT {
     if (!Objects.equals(entity1.getAuthor(), entity2.getAuthor())) {
       return false;
     }
-    return Objects.equals(entity1.getResponsible(), entity2.getResponsible());
+    if (!Objects.equals(entity1.getResponsible(), entity2.getResponsible())) {
+      return false;
+    }
+
+    return true;
   }
 
   private FeatureDto createRandomFullFeatureDto() {
@@ -277,19 +160,6 @@ public class FeatureJaxrsAdapterTestIT {
     return optionDto;
   }
 
-  private void deleteRecord(String location) {
-    RequestSpecification requestDelete =
-            RestAssured.given().auth().basic(OPERATOR_NAME, OPERATOR_PASSWORD);
-
-    List<Integer> featureProcessIdList = getFeatureProcessIdList(location);
-    featureProcessIdList.sort(Comparator.reverseOrder());
-    featureProcessIdList.forEach(featureProcessId -> {
-      requestDelete.delete(location + "/feature-process/" + featureProcessId);
-    });
-    Response deleteResponse = requestDelete.delete(location);
-    Assertions.assertEquals(200, deleteResponse.statusCode()); //TODO: maybe should be 204
-  }
-
   private List<Integer> getFeatureProcessIdList(String location) {
     Response response = RestAssured.given()
             .auth()
@@ -309,82 +179,6 @@ public class FeatureJaxrsAdapterTestIT {
       return list;
     }
     return list;
-  }
-
-  @Disabled
-  @Test
-  public void featureGetTest() {
-    FeatureDto createdDto = getRecord("sasfaf");
-
-  }
-
-  private String postSearchRequest(SearchRequestDto<FeatureSearchDto> searchRequestDto, String location) {
-
-    RequestSpecification request =
-            RestAssured.given().auth().basic(OPERATOR_NAME, OPERATOR_PASSWORD)
-                    .header("content-type", MediaType.APPLICATION_JSON + ";charset=utf-8")
-                    .body(searchRequestDto);
-
-    Response postResponse = request.post(location);
-    Assertions.assertEquals(201, postResponse.statusCode());
-
-    return postResponse.getHeader("location");
-  }
-
-  private SearchRequestDto<FeatureSearchDto> getSearchRequest(String searchLocation) {
-    RequestSpecification request =
-            RestAssured.given().auth().basic(OPERATOR_NAME, OPERATOR_PASSWORD);
-
-    Response response = request.get(searchLocation);
-    Assertions.assertEquals(200, response.getStatusCode());
-
-    return new Gson().fromJson(response.asString(), SearchRequestDto.class);
-  }
-
-  private Integer getFeatureSearchResultSetSize(String location) {
-    String testUrl = location + "/resultset-size";
-
-    RequestSpecification request =
-            RestAssured.given().auth().basic(OPERATOR_NAME, OPERATOR_PASSWORD);
-
-    Response response = request.get(testUrl);
-    Assertions.assertEquals(200, response.getStatusCode());
-
-    return response.getBody().as(Integer.TYPE);
-  }
-
-  private FeatureDto getFeatureResult(String location) {
-    RequestSpecification request =
-            RestAssured.given().auth().basic(OPERATOR_NAME, OPERATOR_PASSWORD);
-
-    Response response = request.get(location);
-
-    Assertions.assertEquals(200, response.statusCode());
-
-    Gson gson = new GsonBuilder().create();
-    return gson.fromJson(response.asString(), FeatureDto.class);
-  }
-
-  @Disabled
-  @Test
-  public void featureSearchTest() {
-    String testUrl = ENTITY_URL + "/search";
-
-    SearchRequestDto<FeatureSearchDto> searchRequestDto = new SearchRequestDto<>();
-
-    FeatureSearchDto searchDto = new FeatureSearchDto();
-    searchDto.setFeatureId(151);
-    searchRequestDto.setTemplate(searchDto);
-
-    String searchLocation = postSearchRequest(searchRequestDto, testUrl);
-
-    SearchRequestDto<FeatureSearchDto> readRequest = getSearchRequest(searchLocation);
-    //TODO: compare SearchRequestDto-s
-
-    if (getFeatureSearchResultSetSize(searchLocation) > 0) {
-      getFeatureResult(searchLocation);
-    }
-
   }
 
 }
