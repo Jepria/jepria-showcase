172635425f8f2221313afe7be9d0a1dd2ace6b6a
dmitrii92 <fogot92@gmail.com>
2020-07-16 10:26:22 +0300

    Fix List form: correct page number in pageFooter.

--- module/JepRiaShowcase/App/client-react/src/pages/feature/view/List.tsx
+++ module/JepRiaShowcase/App/client-react/src/pages/feature/view/List.tsx
@@ -132,6 +132,7 @@ const ListPage = () => {
               </TableBody>
             </Table>
             <JepGridPagingBar
+                currentPage={page}
                 rowCount={pageSize}
                 totalRowCount={searchSize}
                 onRefresh={(pageNumber, pageSize) => {
