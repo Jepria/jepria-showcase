0b6c7e7827840abcdf6db6b6341657b1f2a3efa7
dmitrii92 <fogot92@gmail.com>
2020-12-04 14:21:40 +0300

    fix: OAuth

--- module/JepRiaShowcase/App/client-react/src/features/feature/api/FeatureCrudApi.ts
+++ module/JepRiaShowcase/App/client-react/src/features/feature/api/FeatureCrudApi.ts
@@ -1,11 +1,20 @@
 import { ConnectorCrud } from "@jfront/core-rest";
+import axios, { AxiosInstance } from "axios";
 import { API_PATH } from "../../../config";
 import { Feature, FeatureCreate, FeatureUpdate } from "./FeatureTypes";
 
-class FeatureCrudApi extends ConnectorCrud<Feature, FeatureCreate, FeatureUpdate> {
-  constructor(baseUrl: string) {
-    super(baseUrl, false);
+class FeatureCrudApi extends ConnectorCrud<
+  Feature,
+  FeatureCreate,
+  FeatureUpdate
+> {
+  constructor(
+    baseUrl: string,
+    withCrudentials = false,
+    axiosInstance?: AxiosInstance
+  ) {
+    super(baseUrl, withCrudentials, axiosInstance);
   }
 }
 
-export const featureCrudApi = new FeatureCrudApi(API_PATH + "/feature");
+export const featureCrudApi = new FeatureCrudApi(API_PATH + "/feature", false, axios);
--- module/JepRiaShowcase/App/client-react/src/features/feature/api/FeatureSearchApi.ts
+++ module/JepRiaShowcase/App/client-react/src/features/feature/api/FeatureSearchApi.ts
@@ -1,11 +1,15 @@
 import { Feature, FeatureSearchTemplate } from "./FeatureTypes";
 import { ConnectorSearch } from "@jfront/core-rest";
 import { API_PATH } from "../../../config";
-
+import axios, { AxiosInstance } from "axios";
 class FeatureSearchApi extends ConnectorSearch<Feature, FeatureSearchTemplate> {
-  constructor(baseUrl: string) {
-    super(baseUrl, false);
+  constructor(
+    baseUrl: string,
+    withCrudentials = false,
+    axiosInstance?: AxiosInstance
+  ) {
+    super(baseUrl, withCrudentials, axiosInstance);
   }
 }
 
-export const featureSearchApi = new FeatureSearchApi(API_PATH + "/feature");
+export const featureSearchApi = new FeatureSearchApi(API_PATH + "/feature", false, axios);
