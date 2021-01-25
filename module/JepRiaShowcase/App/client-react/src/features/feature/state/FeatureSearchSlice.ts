810a8e21b663082e8d1054231375d6c3b501ec45
dmitrii92 <fogot92@gmail.com>
2021-01-25 10:36:36 +0300

    chore: change file name

---
+++ module/JepRiaShowcase/App/client-react/src/features/feature/state/FeatureSearchSlice.ts
@@ -0,0 +1,28 @@
+import { createSearchSlice, SearchState } from "@jfront/core-redux-thunk";
+import { Feature, FeatureSearchTemplate } from "../api/FeatureTypes";
+import { featureSearchApi } from "../api/FeatureSearchApi";
+
+export const initialSearchState: SearchState<FeatureSearchTemplate, Feature> = {
+  searchRequest: { template: { maxRowCount: 250 } },
+  error: null,
+  isLoading: false,
+  records: [],
+  searchId: null,
+  pageSize: 25,
+  pageNumber: 1,
+  resultSetSize: null,
+};
+
+export const slice = createSearchSlice({
+  name: "featureSearch",
+  initialState: initialSearchState,
+  reducers: {},
+});
+
+const thunkCreators = slice.thunk;
+
+export const search = thunkCreators.searchThunk(featureSearchApi);
+export const postSearch = thunkCreators.postSearchThunk(featureSearchApi);
+export const postSearchRequest = thunkCreators.postSearchRequestThunk(featureSearchApi);
+
+export const { name, actions, reducer } = slice;
