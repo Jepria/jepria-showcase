759034dfa1014859c138b101c87c743c89b2b114
AlexSav94 <aleksoider@gmail.com>
2021-01-28 20:01:17 +0300

    feat: user meta data

--- module/JepRiaShowcase/App/client-react/src/features/feature/FeatureModuleRoute.tsx
+++ module/JepRiaShowcase/App/client-react/src/features/feature/FeatureModuleRoute.tsx
@@ -1,19 +1,36 @@
-import React from "react";
+import React, {useContext, useEffect, useState} from "react";
 import { Route, useRouteMatch } from "react-router-dom";
 import FeatureRoute from "./FeatureRoute";
 import FeatureProcessRoute from "../feature-process/FeatureProcessRoute";
+import {Forbidden, Loader} from '@jfront/oauth-ui'
+import { UserContext } from '@jfront/oauth-user'
+import {useTranslation} from "react-i18next";
 
 function FeatureModuleRoute() {
+  const { t } = useTranslation();
   const { path } = useRouteMatch();
+  const { isRoleLoading, isUserInRoles } = useContext(UserContext);
+  const [hasAccess, setHasAccess] = useState(false);
+
+  useEffect(() => {
+    isUserInRoles(["JrsEditFeature", "JrsEditAllFeature", "JrsAssignResponsibleFeature"])
+      .then(setHasAccess)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   return (
     <>
-      <Route path={`${path}/:featureId/feature-process`}>
-        <FeatureProcessRoute />
-      </Route>
-      <Route path={`${path}`}>
-        <FeatureRoute />
-      </Route>
+      {isRoleLoading && <Loader title="JepRiaShowcase" text={t("loadingRoles")} />}
+      {!isRoleLoading && !hasAccess && <Forbidden />}
+      {!isRoleLoading && hasAccess && (
+        <>
+          <Route path={`${path}/:featureId/feature-process`}>
+            <FeatureProcessRoute/>
+          </Route>
+          <Route path={`${path}`}>
+            <FeatureRoute />
+          </Route>
+        </>)}
     </>
   );
 }
